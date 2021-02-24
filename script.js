eedcf53046f5b0eb80b99d04ba82cfeb0452e3a6
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-02-24 15:18:24 -0500

    ditched opacity on modal so i could access all squares for click event

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 console.log("sup fam")
 let squares = document.querySelectorAll('.square');
 console.log(squares)
-console.log(squares[1])
 let modal = document.querySelector('.modal');
 console.log(modal.style.opacity);
 let modalClose = document.querySelector('.close');
 console.log(modalClose);
-let mids = document.querySelectorAll('.mid');
-console.log(mids);
+// let mids = document.querySelectorAll('.mid');
+// console.log(mids);
 //query select all squares
 //add event lisener to squares
 //on click event change modal opacity and populate with applicable info from api
@@ -18,13 +17,18 @@ console.log(mids);
 // }
 
 for (let i = 0; i < squares.length; i++) {
-  squares[i].onclick = function () {
-    modal.style.opacity = 1;
-  }
+  squares[i].addEventListener('click', function () {
+    console.log(this)
+    modal.style.display = 'block';
+    //call func here to populate
+  })
 }
-modalClose.onclick = function () {
-  modal.style.opacity = 0;
-}
+
+
+
+modalClose.addEventListener('click', function () {
+  modal.style.display = 'none';
+})
 
 
 // squares.onclick = function() {
@@ -37,3 +41,24 @@ modalClose.onclick = function () {
 //then
 //then > data handler
 //square info = info from from api data.stuff.stuff
+
+
+//iterate onload for each square
+
+let url = 'https://picsum.photos/200';
+fetch(url)
+  .then(response=>{
+    console.log(response)
+    return response.url;
+  })
+  .then (data=> handleResponse(data))
+
+let handleResponse = function (data) {
+  //create node
+  //set node to desired stuff
+  console.log(data)
+}
+
+// let requestPromise = fetch(url)
+
+// requestPromise.then(handleResponse)
