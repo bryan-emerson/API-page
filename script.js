25e1cf91956ff49011b08422159ef55a8a1bc768
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-02-24 11:46:17 -0500

    modal opens on square click

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 console.log("sup fam")
-
 let squares = document.querySelectorAll('.square');
 console.log(squares)
+let modal = document.querySelector('.modal');
+console.log(modal.style.opacity);
 
 //query select all squares
 //add event lisener to squares
 //on click event change modal opacity and populate with applicable info from api
-
+for (let i = 0; i < squares.length; i ++) {
+  squares[i].addEventListener('click', function(e){
+    modal.style.opacity = 1;
+  })
+}
 
 //fetch to url
 //then
