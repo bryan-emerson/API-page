d1101655798abedc84ce58be09c430165d31d7b6
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-02-24 13:15:00 -0500

    modal closes on x click

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 console.log("sup fam")
 let squares = document.querySelectorAll('.square');
 console.log(squares)
+console.log(squares[1])
 let modal = document.querySelector('.modal');
 console.log(modal.style.opacity);
-
+let modalClose = document.querySelector('.close');
+console.log(modalClose);
+let mids = document.querySelectorAll('.mid');
+console.log(mids);
 //query select all squares
 //add event lisener to squares
 //on click event change modal opacity and populate with applicable info from api
-for (let i = 0; i < squares.length; i ++) {
-  squares[i].addEventListener('click', function(e){
+// for (let i = 0; i < mids.length; i ++) {
+//   mids[i].onclick = function() {
+//     modal.style.opacity = 1;
+//   }
+// }
+
+for (let i = 0; i < squares.length; i++) {
+  squares[i].onclick = function () {
     modal.style.opacity = 1;
-  })
+  }
+}
+modalClose.onclick = function () {
+  modal.style.opacity = 0;
 }
 
+
+// squares.onclick = function() {
+//   modal.style.opacity = 1;
+// }
+// modalClose.onclick = function() {
+//   modal.style.opacity = 0;
+// }
 //fetch to url
 //then
 //then > data handler
