f57becfdca0380964bb1474ff2578da6f1b65ed6
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-02-24 20:18:07 -0500

    almost done

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 console.log("sup fam")
 let squares = document.querySelectorAll('.square');
-console.log(squares[0].style.backgroundImage);
-console.log(squares[0].style.backgroundImage)
+//console.log(squares[0].style.backgroundImage);
 let modal = document.querySelector('.modal');
 let modalClose = document.querySelector('.close');
+let modTop = document.querySelector('.modTop');
+//let modTop = document.querySelector('.modTop');
+let modBtm = document.querySelector('.modBtm');
+console.log(modBtm.innerText)
 
-for (let i = 0; i < squares.length; i++) {
-  squares[i].addEventListener('click', function () {
-    console.log(this)
-    modal.style.display = 'block';
-    //call func here to populate
-  })
-}
+// for (let i = 0; i < squares.length; i++) {
+//   squares[i].addEventListener('click', function () {
+//     console.log(this.style)
+//     modal.style.display = 'block';
+//     modTop.style.background = this.style.background
+
+//     //call func here to populate
+//   })
+// }
 
 modalClose.addEventListener('click', function () {
   modal.style.display = 'none';
@@ -21,25 +26,29 @@ modalClose.addEventListener('click', function () {
 let url = 'https://picsum.photos/v2/list?limit=15';
 fetch(url)
   .then(response => response.json())
-  // .then(response=>{
-  //   console.log(response)
-  //   return response.url;
-  // })
   .then(data => handleResponse(data))
 
 let handleResponse = function (data) {
   console.log(data)
   for (let j = 0; j < data.length; j++) {
-    console.log(data[j].url);
+    //console.log(data[j].url);
     //console.log(squares[j].style)
     let url = data[j].download_url.toString()
     //console.log(typeof url)
     squares[j].style.background = `url(${url})`
-    // squares[j].style.background =
     }
   //create node for modal?
   //set node to desired stuff
   //console.log(data[0].url)
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', function () {
+      console.log(this.style)
+      modal.style.display = 'block';
+      modTop.style.background = this.style.background
+      modBtm.innerText = url
+      //call func here to populate
+    })
+  }
 }
 
 
